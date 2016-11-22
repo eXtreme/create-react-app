7dfb1c8a58320886a11c070a5e53fe9898620038
Jacek Jędrzejewski <jacek.jedrzejewski@gmail.com>
2016-11-22 17:47:28 +0100

    expose react_app_env

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -206,7 +206,8 @@ module.exports = {
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
     // In development, this will be an empty string.
     new InterpolateHtmlPlugin({
-      PUBLIC_URL: publicUrl
+      PUBLIC_URL: publicUrl,
+      REACT_APP_ENV: env['process.env'].REACT_APP_ENV || env['process.env'].NODE_ENV
     }),
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -222,7 +222,8 @@ module.exports = {
     // In production, it will be an empty string unless you specify "homepage"
     // in `package.json`, in which case it will be the pathname of that URL.
     new InterpolateHtmlPlugin({
-      PUBLIC_URL: publicUrl
+      PUBLIC_URL: publicUrl,
+      REACT_APP_ENV: env['process.env'].REACT_APP_ENV || env['process.env'].NODE_ENV
     }),
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
