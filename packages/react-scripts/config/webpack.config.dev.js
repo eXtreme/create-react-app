69d2fef75fcb4cea0e4c2b861aeffbcd5a1e538f
Jacek Jędrzejewski <jacek.jedrzejewski@gmail.com>
2017-05-30 12:04:58 +0200

    1.0.6 cssnext?

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -230,20 +230,7 @@ module.exports = {
             options: {
               ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
               plugins: () => [
-                nesting,
-                cssvariables,
-                values,
-                require('postcss-flexbugs-fixes'),
-                autoprefixer({
-                  browsers: [
-                    '>1%',
-                    'last 4 versions',
-                    'Firefox ESR',
-                    'not ie < 9', // React doesn't support IE8 anyway
-                  ],
-                  flexbox: 'no-2009',
-                }),
-                reporter
+                require('postcss-cssnext')()
               ],
             },
           },
